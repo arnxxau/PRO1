8d796093b32488f0bff79616c1bc4225cbf92f4d
arnxxau <arnxxau@gmail.com>
2021-10-13 18:56:00 +0200

    updated README.md

--- P1 - Introduction/P42042v2.cpp
+++ P1 - Introduction/P42042v2.cpp
@@ -8,7 +8,7 @@ int main() {
     char x;
     cin >> x;
     bool is_lower_case = x>='a' and x<='z';
-    bool is_vowel = x=='A' or x=='E' or x=='I' or x=='O' or x=='U'or x=='a' or x=='e' or x=='i' or x=='o' or x=='u';
+    bool is_vowel = x=='A' or x=='E' or x=='I' or x=='O' or x=='U' or x=='a' or x=='e' or x=='i' or x=='o' or x=='u';
 
     if (is_lower_case) cout << "lowercase" << endl;
     else cout << "uppercase" << endl;
